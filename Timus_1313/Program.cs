c235ff7fc5fb0384247fac229a26c84f4a40c679
nsamteladze <nikolai.samteladze@viterahealthcare.com>
2013-03-11 09:23:20 -0400

    Initial commit

---
+++ Timus_1313/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Timus_1313
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
